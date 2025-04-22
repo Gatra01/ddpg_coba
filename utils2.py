c2bffa7a4d1a8463f66778c32172ae709b667453
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-04-22 20:47:56 +0700

    Update utils2.py

--- utils2.py
+++ utils2.py
@@ -37,7 +37,7 @@ class Q_Critic(nn.Module):
         q = self.l3(q)
         return q
 
-def evaluate_policy(channel_gain,state, env, agent, turns = 3,episode):
+def evaluate_policy(channel_gain,state, env, agent,episode,turns = 3):
     env = GameState(5,3)   
     total_scores = 0
     total_data_rate = 0
