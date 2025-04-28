f464455a996a9e91a228aab45bf7672f519f5dbd
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2025-04-28 16:31:16 +0700

    Update env2.py

--- env2.py
+++ env2.py
@@ -13,8 +13,8 @@ class GameState:
         self.observation_space = 2*nodes * nodes + nodes  # interferensi, channel gain, power
         self.action_space = nodes
         self.p = np.random.uniform(0, self.p_max, size=self.nodes)
-        self.lamda=1 #buat punishment reward data rate dan power
-        self.gf = 1.5 #buat growth factor
+        self.lamda=150 #buat punishment reward data rate dan power
+        self.gf = 2 #buat growth factor
         self.efsilon=10e-6 # tolerance convergence
         self.x=1
     def sample_valid_power(self):
